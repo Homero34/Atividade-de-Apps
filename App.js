a5c22caad894ab965f21670b6f482f9d10db0c62
unknown <2324290115@ACADEMICO.IESB.BR>
2025-02-27 10:53:07 -0300

    Criaçao da Estrutura

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+  render() {
+    return(
+        <View>
+          <Text style={{color:'#483D8B', fontSize:65,margin:50,fontWeight:"normal"}}
+          >Aplicacao inicial</Text>
+          <Text>Espaço inicial Aplicaçao</Text>
+          <Text>Versao 1.0</Text>
+
+        <Image
+        
+        source={{uri: 'https://images.pexels.com/photos/29493834/pexels-photo-29493834.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}}
+        style={{width:300, height:300}}
+        />
+
+
+        </View>
+    )
+
+
+  }
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
