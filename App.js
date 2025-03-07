378e3704c42af25c00d32852ab687897981e5002
unknown <2324290115@ACADEMICO.IESB.BR>
2025-03-07 11:17:48 -0300

    Reparos

--- App.js
+++ App.js
@@ -1,91 +1,104 @@
 import React, { Component } from "react";
-import { View, Text, Image, } from 'react-native';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+
+import FuscaIMG from "App_Android/Atividade-de-Apps/assets/Fusca.jpeg"
+import MustangIMG from "App_Android/Atividade-de-Apps/assets/Mustang.jpeg"
+
 
 class App extends Component {
 
   render() {
 
+    const styles = StyleSheet.create({
 
-    const  Fusca = () => {
-
-   
-      return (
-
-        <View style = {{
-          padding: 90 , 
-          margin: 90,
-          backgroundColor: "lightblue",
-          borderRadius: 20,
-          borderStyle: "solid"}}>
-
-          <Image
-          
-          source = {{uri: 'https://images.pexels.com/photos/11883040/pexels-photo-11883040.jpeg?auto=compress&cs=tinysrgb&w=600'}}
-          style = {{width:300 , height:300, borderRadius: 8}}          
-          />
-
-          <Text style = {{color:'#000000'}}>VW - Fusca</Text>
-          <Text style = {{color:'#000000'}}>Ano - 1978</Text>
-          <Text style = {{color:'#000000'}}>Cor - Preto</Text>
-
-          
-        </View>
-    
-      )
-    }
- 
-    const Gol = () => {
-
-      return(
-
-        <View
-        style = {{
-          padding: 90 , 
-          margin: 90,
-          backgroundColor: "lightblue",
-          borderRadius: 20,
-          borderStyle: "solid"}}> 
-
-          <Image
-          
-          source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Volkswagen_Gol_Highline_2023_%2853708009248%29_%28cropped%29.jpg/1200px-Volkswagen_Gol_Highline_2023_%2853708009248%29_%28cropped%29.jpg'}}
-          style = {{width:300 , height:300, borderRadius: 8}}          
-          />
-
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 20}}>VW - Gol</Text>
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 50}}>Ano - 2010</Text>
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 50}}>Cor - Cinza</Text>
+      container: {
+        flex: 1 ,
+        fontSize: 20 ,
+        color: "#000000"
         
-        </View>
+      },
+      card: {
 
-          
+      position: 'relative',
+      borderRadius: 8,
+      backgroundColor: '#fff',
+      marginBottom: 16,
+      shadowColor: 'rgba(0, 0, 0, 0.5)',
+      shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.41,
+      elevation: 2
+      },
+      cardImg: {
+        width: '100%',
+        height: 160,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+      },
+
+    })
 
 
 
-      )
-      
-    }
+    const  carros = [
 
+      {
+        id: "1" ,
+        name: "Fusca",
+        Ano: "1930",
+        Cor: "Amarelo",
+        imagem: FuscaIMG
+      },
+      {
+        id : "2",
+        name: "Mustang",
+        Ano: "1964",
+        Cor: "Preto",
+        imagem: MustangIMG
+      }
+
+
+
+    ]
 
     return(
 
 
 
-        <View style>
+        <View style = {styles.container}>
+          <Text>Aplicacao inicial</Text>
+            <FlatList
+          data={carros}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+           <View style = {styles.card}/>
+           
 
 
 
-          <Text style={{color:'#000000', fontSize:65,margin:50,fontWeight:"normal"}}
-          >Aplicacao inicial</Text>
+
+
+          ) }
+        />
+          
+          
+          
+          
+          
+          
+          
+          
         
-        <Fusca/>
-        <Gol/>
-
+        
         </View>
         
 
   
     )
+    
 
 
   }
