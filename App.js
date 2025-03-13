ffe46ad003c85e3658c1109ddf1bd2cb8566a03c
Arthur Amorim <Arthur>
2025-03-13 18:56:38 -0300

    Finalizando com duvida no tamanho do Texto

--- App.js
+++ App.js
@@ -1,91 +1,108 @@
 import React, { Component } from "react";
-import { View, Text, Image, } from 'react-native';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+
+import FuscaIMG from "./assets/Fusca.png"
+import MustangIMG from "./assets/Mustang.png"
+
 
 class App extends Component {
 
   render() {
 
+    const styles = StyleSheet.create({
 
-    const  Fusca = () => {
-
-   
-      return (
-
-        <View style = {{
-          padding: 90 , 
-          margin: 90,
-          backgroundColor: "lightblue",
-          borderRadius: 20,
-          borderStyle: "solid"}}>
-
-          <Image
-          
-          source = {{uri: 'https://images.pexels.com/photos/11883040/pexels-photo-11883040.jpeg?auto=compress&cs=tinysrgb&w=600'}}
-          style = {{width:300 , height:300, borderRadius: 8}}          
-          />
-
-          <Text style = {{color:'#000000'}}>VW - Fusca</Text>
-          <Text style = {{color:'#000000'}}>Ano - 1978</Text>
-          <Text style = {{color:'#000000'}}>Cor - Preto</Text>
-
-          
-        </View>
-    
-      )
-    }
- 
-    const Gol = () => {
-
-      return(
-
-        <View
-        style = {{
-          padding: 90 , 
-          margin: 90,
-          backgroundColor: "lightblue",
-          borderRadius: 20,
-          borderStyle: "solid"}}> 
-
-          <Image
-          
-          source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Volkswagen_Gol_Highline_2023_%2853708009248%29_%28cropped%29.jpg/1200px-Volkswagen_Gol_Highline_2023_%2853708009248%29_%28cropped%29.jpg'}}
-          style = {{width:300 , height:300, borderRadius: 8}}          
-          />
-
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 20}}>VW - Gol</Text>
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 50}}>Ano - 2010</Text>
-        <Text style = {{color:'#000000', backgroundColor:'#87CEFA', borderRadius: 50}}>Cor - Cinza</Text>
+      container: {
+        flex: 1 ,
+        fontSize: 20 ,
+        color: "#000000"
         
-        </View>
-
-          
-
-
-
-      )
+      },
+      card: {
+        background-clip: content-box,
+      position: "relative",
+      borderRadius: 8,
+      backgroundColor: '#fff',
+      marginBottom: 50,
+      shadowColor: 'rgba(0, 0, 0, 1.5)',
+      shadowOffset: {
+      width: 0,
+      height: 1,
       
-    }
+    },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.2 ,
+      elevation: 2
+      },
+      cardImg: {
+        width: '10%',
+        height: 160,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+      },
+      title: {
+        fontSize: 20,
+        fontFamily: "BebasNeue-Regular",
+        fontWeight: "bold",
+        textAlign: "left",
+        marginBottom: 20,
+      }
 
+    })
+
+
+
+    const  carros = [
+
+      {
+        id: "1" ,
+        name: "Fusca",
+        Ano: "1930",
+        Cor: "Amarelo",
+        image: FuscaIMG,
+      },
+      {
+        id : "2",
+        name: "Mustang",
+        Ano: "1964",
+        Cor: "Preto",
+        image: MustangIMG,
+      }
+
+
+
+    ];
 
     return(
 
 
 
-        <View style>
+        <View style = {styles.container}>
+          <Text style={styles.title}>Aplicacao de Carros</Text>
+            <FlatList
+          data={carros}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+          <View style = {styles.card}>
+           <Image source={item.image} style={styles.cardImg}/>
+           <View style={styles.info}>
+              <Text style={styles.carName}>{item.name}</Text>
+              <Text>Ano: {item.Ano}</Text>
+              <Text>Cor: {item.Cor}</Text>
+           </View>
+          </View>
 
 
 
-          <Text style={{color:'#000000', fontSize:65,margin:50,fontWeight:"normal"}}
-          >Aplicacao inicial</Text>
-        
-        <Fusca/>
-        <Gol/>
 
+
+          ) }
+        />
         </View>
         
 
   
     )
+    
 
 
   }
